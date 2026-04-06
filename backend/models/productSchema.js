5fcad6021cdbe19576a9c1a9f1b205fb6d19ec95
Rutvik Mistry <135792228+Rutvik1546@users.noreply.github.com>
2026-04-06 16:24:19 +0530

    Update productSchema.js

--- backend/models/productSchema.js
+++ backend/models/productSchema.js
@@ -1,1 +1,54 @@
+const mongoose = require('mongoose');
 
+const productSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        trim : true
+    },
+
+    category : {
+        type : String,
+        required : true,
+        trim : true,
+        lowercase : true
+    },
+
+    variants : {
+        type : [{
+            size : {
+                type : String,
+                required : true
+            },
+            price : {
+                type : String,
+                required : true
+            }
+        }]
+    },
+    
+    isAvailable : {
+        type : Boolean,
+        default : true
+    }
+}, {timestamps : true});
+
+module.exports = mongoose.model("Product",productSchema);
+
+
+// {
+//     "name" : "Pizza",
+//     "category" : "Food",
+//     "variants" : [ {
+
+//         "name" : "Small",
+//         "price" : 100
+//     }, {
+//         "name" : "Medium",
+//         "price": 150
+//     } , {
+//         "name" : "Large",
+//         "price" : 200
+//     }]
+    
+// }
