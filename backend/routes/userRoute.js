21a2305108da20970cebe964c36b73459635bf18
Rutvik Mistry <135792228+Rutvik1546@users.noreply.github.com>
2026-04-06 16:26:08 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,1 +1,55 @@
+const express = require("express");
+const router = express.Router();
 
+const {
+    getMe,
+    getAllUsers,
+    getOneUser,
+    updateUser,
+    deleteUser,
+    restoreUser
+} = require("../controllers/userController");
+
+const {
+    isAuthenticated,
+    authorizeRoles
+} = require("../middlewares/authMiddleware");
+
+router.get("/me", isAuthenticated, getMe);
+
+router.get(
+    "/",
+    isAuthenticated,
+    authorizeRoles("admin"),
+    getAllUsers
+);
+
+router.get(
+    "/:id",
+    isAuthenticated,
+    authorizeRoles("admin"),
+    getOneUser
+);
+
+router.put(
+    "/:id",
+    isAuthenticated,
+    authorizeRoles("admin","user"),
+    updateUser 
+);
+
+router.delete(
+    "/:id",
+    isAuthenticated,
+    authorizeRoles("admin"),
+    deleteUser
+);
+
+router.put(
+    "/restore/:id",
+    isAuthenticated,
+    authorizeRoles("admin"),
+    restoreUser
+);
+
+module.exports = router;
