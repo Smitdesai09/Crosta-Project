8896d1e77336e6801f1742308002f5da3b102534
Rutvik Mistry <135792228+Rutvik1546@users.noreply.github.com>
2026-04-06 16:13:56 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,1 @@
-const mongoose = require("mongoose");
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Database Connected Successfully!");
-    } catch(error) {
-        console.error(error.message);
-    }
-}
-
-module.exports = connectDB;
