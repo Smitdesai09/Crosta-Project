7122387cf0aee5369878a270bc36610b0e22e192
Rutvik Mistry <135792228+Rutvik1546@users.noreply.github.com>
2026-04-23 09:08:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,10 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    // origin: process.env.CLIENT_URL || "http://localhost:3000",
-    // origin: "https://crosta-project-lcploy10q-rutvik1546s-projects.vercel.app/",
-    origin:true,
-    credentials: true //  allows cookies
+  origin: "https://crosta-project-3.onrender.com",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.options("*", cors());
