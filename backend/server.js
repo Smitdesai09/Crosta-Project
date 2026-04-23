8193b882f883a13bb955a901dfb3d531e42e0134
Rutvik Mistry <135792228+Rutvik1546@users.noreply.github.com>
2026-04-23 09:22:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,13 @@ const app = express()
 app.use(cookieParser());
 app.use(express.json());
 
-app.use(cors({
-  origin: "https://crosta-project-3.onrender.com",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://crosta-project-3.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 app.options("*", cors());
 
 //routes
